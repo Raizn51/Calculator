a0e554339055c55603020fb0ff3c2d3c1631d498
Raizn51 <gouravraikwar989@gmail.com>
2025-01-07 12:53:21 +0530

    multipliaction added

--- Calculator.java
+++ Calculator.java
@@ -10,27 +10,43 @@ class Calculator
 			System.out.println("Addition	:	1\nSubstraction	:	2\nMultiply	:	3\nDivision	:	4\nExit		:	5");
 			System.out.print("Give an input	:	");
 			int input = sc.nextInt();
-			System.out.print("Enter First Number	:	");
-			int F1 = sc.nextInt();
-			System.out.print("Enter Second Number	:	");
-			int F2 = sc.nextInt();
 			int result;
-			switch(input)
+			if(input<0||input>5)
 			{
-				case 1:
-				{
-					result=F1+F2;
-					System.out.println("Result	:	"+result);
-					break;
-				}
-				case 2:
-				{
-					result=F1-F2;
-					System.out.println("Result	:	"+result);
-					break;
-				}
-				
+				System.out.println("Please enter valid input");
+
 			}
+			else
+			{
+				System.out.print("Enter First Number	:	");
+				int F1 = sc.nextInt();
+				System.out.print("Enter Second Number	:	");
+				int F2 = sc.nextInt();
+				switch(input)
+				{
+				
+					case 1:
+					{
+						result=F1+F2;
+						System.out.println("Result	:	"+result);
+						break;
+					}
+					case 2:
+					{
+						result=F1-F2;
+						System.out.println("Result	:	"+result);
+						break;
+					}
+					case 3:
+					{
+						result=F1*F2;
+						System.out.println("Result	:	"+result);
+						break;
+					}
+				}
+			}
+			
+			
 			System.out.println("::::::::::::::::::::::::::::::");
 			break;
 			
