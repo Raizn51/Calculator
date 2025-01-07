7e21e15a747e1227f7dd4d192cc9a3d765cc8883
Raizn51 <gouravraikwar989@gmail.com>
2025-01-07 12:15:10 +0530

    Addition

--- Calculator.java
+++ Calculator.java
@@ -3,9 +3,30 @@ class Calculator
 {
 	public static void main(String []args)
 	{
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to Calculator");
-		System.out.println("Addition	:	1\nSubstraction	:	2\nMultiply	:	3\nDivision	:	4\nExit		:	5");
-		
+		while(true)
+		{
+			System.out.println("Addition	:	1\nSubstraction	:	2\nMultiply	:	3\nDivision	:	4\nExit		:	5");
+			System.out.print("Give an input	:	");
+			int input = sc.nextInt();
+			System.out.print("Enter First Number	:	");
+			int F1 = sc.nextInt();
+			System.out.print("Enter Second Number	:	");
+			int F2 = sc.nextInt();
+			switch(input)
+			{
+				case 1:
+				{
+					System.out.println("Result	:	"+(F1+F2));
+					break;
+				}
+				
+			}
+			System.out.println("::::::::::::::::::::::::::::::");
+			break;
+			
+		}
 		
 		
 		
