edf410f2b9537767bd7c657d9e6f453e5d6759ec
Raizn51 <gouravraikwar989@gmail.com>
2025-01-07 11:45:26 +0530

    Messages

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 class Calculator
 {
 	public static void main(String []args)
 	{
 		System.out.println("Welcome to Calculator");
+		System.out.println("Addition	:	1\nSubstraction	:	2\nMultiply	:	3\nDivision	:	4\nExit		:	5");
+		
+		
+		
+		
 	}
 }
